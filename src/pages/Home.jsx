fcfd2e2b05e5cfb1534eff2b72c93ba04a1f7e1c
jobetancur <alejobetancur2@gmail.com>
2022-09-12 14:23:47 -0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,138 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { db, auth, storage } from '../firebase'
+import { collection, query, where, onSnapshot, addDoc, Timestamp, orderBy, setDoc, doc, getDoc, updateDoc } from 'firebase/firestore'
+import User from '../components/User'
+import MessageForm from '../components/MessageForm'
+import { ref, getDownloadURL, uploadBytes } from 'firebase/storage'
+import Message from '../components/Message'
 
 const Home = () => {
+
+  const [users, setUsers] = useState([])
+  const [chat, setChat] = useState('')
+  const [text, setText] = useState('')
+  const [img, setImg] = useState('')
+  const [msgs, setMsgs] = useState([])
+
+  const user1 = auth.currentUser.uid
+
+  useEffect(() => {
+    const usersRef = collection(db, 'users')
+    // Create query object
+    const q = query(usersRef, where('uid', 'not-in', [user1]))
+    // Execute query
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let users = []
+      querySnapshot.forEach(doc => {
+        users.push(doc.data())
+      })
+      setUsers(users)
+    })
+    return () => unsub()
+  }, [])
+  
+  const selectUser = async (user) => {
+    setChat(user)
+    console.log(user);
+
+    const user2 = user.uid
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+    const msgsRef = collection(db, 'messages', id, 'chat')
+    const q = query(msgsRef, orderBy('createdAt', 'asc'))
+
+    onSnapshot(q, querySnapshot => {
+      let msgs = []
+      querySnapshot.forEach(doc => {
+        msgs.push(doc.data())
+      })
+      setMsgs(msgs)
+    })
+    const docSnap = await getDoc(doc(db, 'lastMsg', id))
+    if (docSnap.data()?.from !== user1) {
+      await updateDoc(doc(db, 'lastMsg', id), {
+        unread: false
+      })
+    }
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const user2 = chat.uid
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+    let url;
+    if (img) {
+      const imgRef = ref(
+        storage,
+        `images/${new Date().getTime()} - ${img.name}`
+      )
+      const snap = await uploadBytes(imgRef, img)
+      const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath))
+      url = dlUrl
+    }
+
+    await addDoc(collection(db, 'messages', id, 'chat'), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || ''
+    })
+
+    await setDoc(doc(db, 'lastMsg', id), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || '',
+      unread: true
+    })
+
+    setText('')
+  }
+
   return (
     <div>
-        Home Page
+      <div className="home_container">
+        <div className="users_container">
+          {users.map(user =>
+            <User
+              key={user.id}
+              user={user}
+              selectUser={selectUser}
+              user1={user1}
+              chat={chat}
+            />
+          )}  
+        </div>
+        <div className="messages_container">
+          {
+            chat ?
+            <>
+            <div className='messages_user'>
+              <h3> {chat.name} </h3>    
+            </div>
+            <div className="messages">
+                  {msgs.length ? msgs.map((msg, index) =>
+                    <Message
+                      key={index}
+                      msg={msg}
+                      user1={user1} 
+                    />)
+                    :
+                    null}      
+            </div>
+            <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} setImg={setImg} />    
+            </>
+              :
+            <div>
+              <h3 className='no_conv'>Select a user to initiate a conversation</h3>    
+            </div>  
+          }
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 import profile_image from '../img/profile_image.png'
 import Camera from '../svg/Camera'
 import { storage, db, auth } from '../firebase'
-import { ref, getDownloadURL, uploadBytes } from 'firebase/storage'
+import { ref, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage'
 import { getDoc, doc, updateDoc } from 'firebase/firestore'
+import Trash from '../svg/Trash'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
 
     const [img, setImg] = useState('')
     const [user, setUser] = useState()
+    const navigate = useNavigate()
     
     useEffect(() => {
         getDoc(doc(db, 'users', auth.currentUser.uid)).then(docSnap => {
@@ -21,12 +24,15 @@ const Profile = () => {
             const uploadImg = async () => {
                 const imgRef = ref(storage, `avatar/${new Date().getTime()} - ${img.name}`);
                 try {
+                    if (user.avatarPath) {
+                        await deleteObject(ref(storage, user.avatarPath))
+                    }
                     const snap = await uploadBytes(imgRef, img)
                     const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
                 
                     await updateDoc(doc(db, 'users', auth.currentUser.uid), {
                         avatar: url,
-                        avatarPath: snap.ref.fullpath,
+                        avatarPath: snap.ref.fullPath,
                     });
                     setImg('');
                     console.log(url);
@@ -38,16 +44,33 @@ const Profile = () => {
         };
     }, [img])
 
+    const delenteImage = async () => {
+        try {
+            const confirm = window.confirm('Delete avatar?')
+            if (confirm) {
+                await deleteObject(ref(storage, user.avatarPath))
+                await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+                    avatar: '',
+                    avatarPath: ''
+                })
+                navigate('/')
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
   return user ? (
     <section className='display_container'>
         <div className="profile_container">
             <div className="img_container">
-                <img src={profile_image || user.avatar} alt="avatar" />
+                <img src={user.avatar || profile_image} alt="avatar" />
                 <div className="overlay">
                     <div>
                         <label htmlFor="photo">
                             <Camera/>
-                        </label>
+                          </label>
+                          {user.avatar ? <Trash delenteImage={delenteImage} /> : null}
                         <input 
                             type="file" accept='image/*' 
                             style={{ display: 'none' }} 
